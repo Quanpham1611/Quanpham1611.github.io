0928f9069814d699fcc9f29113a86e5e1c27f519
quan <qpham16112003@gmail.com>
2023-07-14 12:04:30 +0700

    jij

---
+++ cms-student-side/build/web/coursedetailenrolled.jsp
@@ -0,0 +1,182 @@
+<%-- 
+    Document   : coursedetailenrolled
+    Created on : Jun 22, 2023, 7:36:32 AM
+    Author     : Dell
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>CMS - Course Management System</title>
+        <link rel="shortcut icon" href="https://cmshn.fpt.edu.vn/theme/image.php/trema/theme/1684246329/favicon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="header" style="height: 27px; display: flex; align-items: center;">
+            <div class="hamburger-menu">
+                <input id="menu__toggle" type="checkbox" />
+                <label class="menu__btn" for="menu__toggle">
+                    <span></span>
+                </label>
+
+                <ul class="menu__box">
+                <li><a class="menu__item" href="index.html"><i class="fa fa-home"></i>Trang chủ</a></li>
+                <li><a class="menu__item" href="allcourse"><i class="fa fa-book"></i>Tất cả khóa học</a></li>
+
+
+            </ul>
+            </div>
+            <img src="https://cmshn.fpt.edu.vn/pluginfile.php/1/core_admin/logocompact/0x70/1684246329/2020-FPTU-Eng.png" alt="logo" style="height: 27px; margin-left: 40px;">
+            <h2 style="margin-left: 10px; color: red">${courseName}</h2>
+        </div>
+
+        <div class="content-body">
+            <h3 style="color: red">Uploads File</h3>
+
+            <c:forEach items="${content}" var="courseContent">
+                <div class="square-box" >
+                    <a href="download?name=${courseContent.contentName}" style="color: black; text-decoration: none; font-size: 30px">${courseContent.contentName}</a><br>
+                </div>
+            </c:forEach>
+            <c:forEach items="${exam}" var="courseExam">
+                <div class="square-box" >
+                    <a href="submitfile.jsp?examName=${courseExam.examName}&courseName=${courseName}" style="color: black; text-decoration: none; font-size: 30px" onclick="showRemainingTime('${courseExam.finish}')">
+                        Bạn có bài kiểm tra: ${courseExam.examName}(bắt đầu: ${courseExam.begin} - kết thúc: ${courseExam.finish})
+                    </a><br>
+                </div>
+            </c:forEach>
+
+        </div>
+
+    </body>
+    <style>
+        #menu__toggle {
+            opacity: 0;
+        }
+        #menu__toggle:checked + .menu__btn > span {
+            transform: rotate(45deg);
+        }
+        #menu__toggle:checked + .menu__btn > span::before {
+            top: 0;
+            transform: rotate(0deg);
+        }
+        #menu__toggle:checked + .menu__btn > span::after {
+            top: 0;
+            transform: rotate(90deg);
+        }
+        #menu__toggle:checked ~ .menu__box {
+            left: 0 !important;
+        }
+        .menu__btn {
+            position: fixed;
+            top: 20px;
+            left: 20px;
+            width: 26px;
+            height: 26px;
+            cursor: pointer;
+            z-index: 1;
+        }
+        .menu__btn > span,
+        .menu__btn > span::before,
+        .menu__btn > span::after {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 2px;
+            background-color: #616161;
+            transition-duration: .25s;
+        }
+        .menu__btn > span::before {
+            content: '';
+            top: -8px;
+        }
+        .menu__btn > span::after {
+            content: '';
+            top: 8px;
+        }
+        .menu__box {
+            display: block;
+            position: fixed;
+            top: 0;
+            left: -100%;
+            width: 300px;
+            height: 100%;
+            margin: 0;
+            padding: 80px 0;
+            list-style: none;
+            background-color: #ECEFF1;
+            box-shadow: 2px 2px 6px rgba(0, 0, 0, .4);
+            transition-duration: .25s;
+        }
+        .menu__item {
+            display: block;
+            padding: 12px 24px;
+            color: #333;
+            font-family: 'Roboto', sans-serif;
+            font-size: 20px;
+            font-weight: 600;
+            text-decoration: none;
+            transition-duration: .25s;
+        }
+        .menu__item:hover {
+            background-color: #CFD8DC;
+        }
+        .subject {
+            width: 200px;
+            height: auto;
+            border: 1px solid black;
+            margin: 10px;
+            padding: 10px;
+        }
+        .content-body{
+            width: 100%;
+            height: 100%;
+            border: 1px solid #000;
+        }
+        .square-box {
+            width: 100%; /* Độ rộng của hình vuông */
+            height: 50px; /* Chiều cao của hình vuông */
+            display: flex;
+            align-items: center;
+            border: 1px solid #000;
+        }
+
+        .square-box a {
+            display: block;
+            text-align: center;
+            color: #000;
+        }
+    </style>
+    <script>
+        function showRemainingTime(finishTime) {
+            var currentTime = new Date();
+            var endTime = new Date(finishTime);
+
+            var remainingTime = endTime - currentTime;
+
+            // Chuyển đổi thời gian còn lại thành giây
+            var remainingSeconds = Math.floor(remainingTime / 1000);
+
+            if (remainingSeconds > 0) {
+                var remainingMinutes = Math.floor(remainingSeconds / 60);
+                var remainingHours = Math.floor(remainingMinutes / 60);
+                var remainingDays = Math.floor(remainingHours / 24);
+
+                remainingHours %= 24;
+                remainingMinutes %= 60;
+                remainingSeconds %= 60;
+
+                var alertMessage = "Bạn còn " + remainingDays + " ngày, " + remainingHours + " giờ, " + remainingMinutes + " phút, " + remainingSeconds + " giây để làm bài kiểm tra.";
+
+                alert(alertMessage);
+            } else {
+                alert("Thời gian làm bài kiểm tra đã hết.");
+            }
+        }
+    </script>
+
+</html>
---
+++ cms-student-side/src/java/document/Document.java
@@ -0,0 +1,146 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package document;
+
+/**
+ *
+ * @author Dell
+ */
+public class Document {
+    /*
+    Chức năng 1: Đăng nhập - Đăng Xuất (dùng OAuth 2.0)
+    - Tạo databasse mới tên cms: create database cms
+    - Tạo bảng Users với các thuộc tính: String id, String email, String name, String given_name,
+    String family_name,  String picture;
+    CREATE TABLE Users (
+	ID NVARCHAR(256) PRIMARY KEY,
+	Email NVARCHAR(250) NOT NULL,
+	Name NVARCHAR(250) NOT NULL,
+	Given_name NVARCHAR(250) NOT NULL,
+	Family_name NVARCHAR(250) NOT NULL,
+	Picture NVARCHAR(250) NOT NULL,
+	enrollCourseId NVARCHAR(256) REFERENCES Course(CourseId)
+    );
+ 
+    - Đầu tiên yêu cầu người dùng đăng nhập bằng mail @fpt.edu.vn
+        +) Nếu đúng mail thì sẽ kiểm tra người dùng co trong database chưa bằng câu lệnh SELECT * FROM Users WHERE Email = '" + email + "'"
+            . Nếu chưa thì sẽ tự động thêm những thuộc tính ở bảng Users vào database bằng câu lệnh
+                INSERT INTO Users (ID ,Email, Name, Given_Name, Family_Name, Picture) VALUES (?, ?, ?, ?, ?, ?)
+            . Nếu người dùng đã tồn tại thì sẽ chuyển người dùng sang trang doashboard.jsp
+        +) Nếu người dùng không dùng mail @fpt.edu.vn thì sẽ chuyển người dùng về trang login.jsp yêu cầu đăng nhập lại
+    
+    CHức năng 2: HIển thị toàn bộ khóa học và khóa học người dùng đã enroll
+    - Sau khi người dùng đăng nhập thành công vào trang doashboard.jsp ở đây sẽ hiển thị menu show ra những khóa học người dùng
+    đã enroll, nếu người dùng chưa enroll thì hiển thị ra rỗng
+    - Tạo bảng Course trong database bằng câu lệnh
+    create table Course (
+	CourseId nvarchar(256) primary key,
+	CourseName nvarchar(250) not null,
+	Semester nvarchar(250) not null,
+	TeacherName nvarchar(250) not null,
+        Picture nvarchar(1000) not null,
+    )
+    - Thêm thông tin vào bảng Course trong database
+        insert into Course values
+            ('SSL101c','Academic Skills for University Success','FA23','HanhPN', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('CSI104','Introduction to Computer Science','FA23','AnhTD', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('PRF192','Programming Fundamentals','FA23','ThoPN', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/08/dai-hoc-fpt-tp-hcm-1.jpeg'),
+            ('MAE101','	Mathematics for Engineering','FA23','NguyenDA', 'https://hcmuni.fpt.edu.vn/Data/Sites/1/media/1-2021-huynh-anh/hoc-phi-dh-fpt-2022/dji_0290.jpg'),
+            ('CEA201','Computer Organization and Architecture','FA23','BanTQ', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('PRO192','Object-Oriented Programming','FA23','TuanVM', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('MAD101','Discrete mathematics','FA23','AnhNN', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/08/dai-hoc-fpt-tp-hcm-1.jpeg'),
+            ('OSG202','Operating Systems','FA23','AnhDN', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('NWC203c','Computer Networking','FA23','MinhTH', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('SSG104','Communication and In-Group Working Skills','FA23','QuanN', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('JPD113','Elementary Japanese 1-A1.1','FA23','LinhNT', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/08/dai-hoc-fpt-tp-hcm-1.jpeg'),
+            ('CSD201','	Data Structures and Algorithms','FA23','TruongNQ', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('DBI202','Introduction to Databases','FA23','TienTD', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('LAB211','OOP with Java Lab','FA23','NangNT', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('WED201c','Web Design','FA23','ThoPN', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('MAS291','Statistics and Probability','FA23','HongNN', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('JPD123','Elementary Japanese 1-A1.2','FA23','CauPD', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg'),
+            ('IOT102','Internet of Things','FA23','LinhLTT', 'https://images2.thanhnien.vn/Uploaded/trinm/2023_01_04/fpthanoi01-130.jpg'),
+            ('PRJ301','Java Web Application Development','FA23','TienTD', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/08/dai-hoc-fpt-tp-hcm-1.jpeg'),
+            ('SWE201c','Introduction to Software Engineering','FA23','LongNT', 'https://daihoc.fpt.edu.vn/wp-content/uploads/2022/02/HCM-scaled.jpeg');
+    - Tạo bảng UserCourse để lưu vào database tài khoản có Id nào đã tham gia khóa học nào bằng lệnh
+        create table UserCourse (
+        UserId nvarchar(256),
+        CourseId nvarchar(256),
+        primary key (UserId, CourseId),
+        foreign key (UserId) references Users(ID),
+        foreign key (CourseId) references Course(CourseId)
+        );
+    - Sau đó hiển thị ra bằng câu lệnh SELECT UC.CourseId, C.CourseName FROM UserCourse UC JOIN Course C ON UC.CourseId = C.CourseId WHERE UC.UserId = ?
+    - HIển thị toàn bộ khóa học bằng câu lệnh: SELECT * FROM COURSE
+    
+    CHức năng 3: Tham gia khóa học
+    - Khi thực hiện tìm kiếm hay hiển thị toàn bộ khóa học nếu như người dùng chưa tham gia khóa học đó thì sẽ được yêu cầu tham gia khóa học
+    bằng câu lệnh INSERT INTO UserCourse (UserId, CourseId) VALUES (?, ?)
+    - Nếu như tham gia thành công sẽ dẫn đến trang coursedetailenrolled, còn không thì quay ngược lại trang allcourse
+    
+    Chức năng 4: Tìm kiếm khóa học
+    - Người dùng có thể tìm kiếm khóa học dựa vào tên khóa học, mã khóa học hoặc là dựa vào tên giảng viên
+    - Nếu từ khóa để tìm kiếm đúng hoặc trong tên khóa học, mã khóa học, tên giảng viên chứa cụm từ khóa thì sẽ show ra kết quả
+    - Nếu không có thì sẽ quay trở lại trang search và in ra thông báo để cho người dùng tìm kiếm lại
+    
+    Chức năng 5: HIển thị nội dung khóa học
+    - Tạo bảng CourseContent bằng câu lệnh
+        CREATE TABLE CourseContent (
+        ContentId NVARCHAR(256) PRIMARY KEY,
+        CourseId NVARCHAR(256) REFERENCES Course(CourseId),
+        FileName NVARCHAR(250) NOT NULL,
+        FilePath NVARCHAR(1000) NOT NULL,
+        );
+    - Thêm nội dung khóa học vào database
+        insert into CourseContent
+        values ('1', 'PRF192','Baitap.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\Baitap.docx'),
+        ('2', 'PRF192','Diem.xlsx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\Diem.xlsx'),
+        ('3', 'PRF192','Moitruong.zip','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\Moitruong.zip'),
+        ('4', 'PRF192','SlideSlot1.pptx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\SlideSlot1.pptx'),
+        ('5', 'PRF192','TinhDiem.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\TinhDiem.docx'),
+        ('6', 'MAE101','SlideToan.pptx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\SlideToan.pptx'),
+        ('7', 'MAE101','DiemToan.xlsx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\DiemToan.xlsx'),
+        ('8', 'MAE101','BaiTapToan.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\BaiTapToan.docx'),
+        ('9', 'PRO192','PRO192.zip','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\PRO192.zip'),
+        ('10', 'PRO192','HuongDanCaiMoiTruong.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\HuongDanCaiMoiTruong.docx'),
+        ('11', 'PRO192','DiemPro192.xlsx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\DiemPro192.xlsx'),
+        ('12', 'PRO192','BaitapPro.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\BaitapPro.docx'),
+        ('13', 'MAD101','MAD.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\MAD.docx'),
+        ('14', 'MAD101','MADPoint.xlsx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\MADPoint.xlsx'),
+        ('15', 'MAD101','MADSlide.pptx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\MADSlide.pptx'),
+        ('16', 'MAD101','BaiTapMAD.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\BaiTapMAD.docx'),
+        ('17', 'JPD113','BangChuCai.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\BangChuCai.docx'),
+        ('18', 'JPD113','TrangTuHA.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\TrangTuHA.docx'),
+        ('19', 'CSD201','CSD_Source.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\CSD_Source.docx'),
+        ('20', 'CSD201','DiemCSD.docx','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\DiemCSD.docx'),
+        ('21', 'CSD201','SlideCSD.zip','C:\\Users\\Dell\\Documents\\NetBeansProjects\\cms-student-side\\uploads\\SlideCSD.zip')
+    - Sau khi người dùng click vào bất kì file nào file đó sẽ tự động tải về
+    - Hiện tại mới chỉ dừng ở việc hỗ trợ các loại file docx, xlsx, pptx, zip, rar
+    Chức năng 6: Làm bài kiểm tra trong khóa học
+    - Tạo bảng CourseExam
+        CREATE TABLE CourseExam (
+        ExamName NVARCHAR(256) PRIMARY KEY,
+        CourseId NVARCHAR(256) REFERENCES Course(CourseId),
+	ExamBegin Datetime,
+        ExamFinish Datetime,
+    );
+    - Khi khóa học nào có bài kiểm tra thì sẽ hiện thị lên cho người dùng biết
+    - Khi làm bài xong người dùng sẽ nộp lại file bài tập
+    - Tạo bảng
+    create table SubmitExam(
+	UserId nvarchar(256) references Users(ID),
+	CourseId nvarchar(256) references Course(CourseId),
+	ExamName nvarchar(256),
+	FileName nvarchar(256) not null,
+	FilePath nvarchar(256) not null
+    );
+    - Sau khi người dùng nộp thành công phải có 2 yêu cầu
+    +) file-path đã được lưu vào database
+    +) folder uploads-std đã có file đó
+    đáp ứng đủ 2 yêu cầu trên chức năng hoàn thành
+
+     */
+
+}
